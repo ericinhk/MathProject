eb16d7dd71633aa8eb941fb9d62ee701ff5fa536
Eric <Eric@ERIC-PC>
2019-03-10 16:19:19 +0800

    commit 1

---
+++ MathTest/src/com/mytest/app/MathApp.java
@@ -0,0 +1,9 @@
+package com.mytest.app;
+
+public class MathApp {
+	public static void main(String[] args) {
+		System.out.println("Math Application");
+		
+		System.out.println("2 X 2 = "+(2*2));
+	}
+}
